5258459913b27792f2e246e43cd4f21304ee23cf
qwwertyz <167813551+qwwertyz@users.noreply.github.com>
2025-12-23 16:38:08 +0000

    changed cursor, added fixed go-to-top button

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const btn = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
